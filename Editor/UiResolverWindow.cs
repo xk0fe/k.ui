7e9f92610a2745081a466c76011d2db5de3c3367
k0fe <x.k0fe@yandex.ru>
2024-11-13 00:18:27 -0300

    add viewbase auto resolver

---
+++ Editor/UiResolverWindow.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using Packaged.k.UI.Configs;
+using Packaged.k.UI.Views;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Source.Scripts.Client.UI.Editor
+{
+    public class UiResolverWindow : EditorWindow
+    {
+        private UiConfig _config;
+        
+        [MenuItem("k/UI/UiAutoResolver")]
+        public static void Open()
+        {
+            var wnd = GetWindow<UiResolverWindow>();
+            wnd.titleContent = new GUIContent("UiAutoResolver");
+        }
+        
+        public void CreateGUI()
+        {
+            var root = rootVisualElement;
+
+            CreateDescription(root);
+            CreateConfig(root);
+            CreateButton(root);
+        }
+
+        private void CreateDescription(VisualElement root)
+        {
+            var header = new Label("Resolve UI references")
+            {
+                style =
+                {
+                    fontSize = 16
+                }
+            };
+            root.Add(header);
+            var description = new Label("Find all UI prefabs that have ViewBase component and add them to the config.")
+            {
+                style =
+                {
+                    flexGrow = 1,
+                    whiteSpace = WhiteSpace.Normal,
+                    maxWidth = Length.Percent(100),
+                    marginTop = 10
+                }
+            };
+            root.Add(description);
+        }
+
+        private void CreateConfig(VisualElement root)
+        {
+            var config = new ObjectField("Config")
+            {
+                objectType = typeof(UiConfig)
+            };
+            config.RegisterValueChangedCallback(evt =>
+            {
+                _config = evt.newValue as UiConfig;
+            });
+            root.Add(config);
+        }
+
+        private void CreateButton(VisualElement root)
+        {
+            var button = new Button
+            {
+                name = "button",
+                text = "Resolve"
+            };
+            button.clicked += OnButtonClick;
+            root.Add(button);
+        }
+
+        private void OnButtonClick()
+        {
+            if (_config == null)
+            {
+                UnityEngine.Debug.LogError("Config is null");
+                return;
+            }
+            var prefabs = AssetDatabase.FindAssets("t:Prefab");
+            var views = new List<ViewBase>();
+            foreach (var prefab in prefabs)
+            {
+                var path = AssetDatabase.GUIDToAssetPath(prefab);
+                var asset = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+                if (asset.TryGetComponent<ViewBase>(out var viewBase))
+                {
+                    views.Add(viewBase);
+                }
+            }
+            
+            _config.SetViews(views);
+            EditorUtility.SetDirty(_config);
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
